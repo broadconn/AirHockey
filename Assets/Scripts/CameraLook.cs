d7a2229b85626c29e944d0dce34a80884bed0a78
Connor Broad <connor.broad@gmail.com>
2022-09-12 22:16:51 +1200

    Camera look tweaks

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
-public class CameraLook : MonoBehaviour
-{
-    [SerializeField] Transform lookAt;
-    [SerializeField] float lookSpeed = 10;
+public class CameraLook : MonoBehaviour {
+    [SerializeField] Transform defaultLookPos;
+    [SerializeField] Transform lookTarget;
+    [SerializeField] float lookSpeed = 1;
+    [SerializeField] float deviationLimit = 2;
 
-    private void LateUpdate() { 
-        var targetRotation = Quaternion.LookRotation(lookAt.position / 10 - transform.position);
+    private void LateUpdate() {
+        var defToLook = lookTarget.position - defaultLookPos.position;
+        var tgtPos = defaultLookPos.position + defToLook * defToLook.magnitude / deviationLimit;
+        var targetRotation = Quaternion.LookRotation(tgtPos - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
     }
 }
